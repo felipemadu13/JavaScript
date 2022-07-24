b181f2a12b94cc6e1fd547b8c2b206ffdb46fa53
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-07-24 17:36:11 -0300

    Tipo Booleano

--- type-string.js
+++ type-string.js
@@ -1,3 +1,13 @@
-const texto = 'Olá, mundo!'
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "SenhaSuperSegura456!";
+const StringDeNumeros = "34567";
 
-console.log(texto)
+const citacao = 'Meu nome é ';
+const nome = 'Felipe'
+
+// Concatenação (+)
+
+console.log(citacao + nome)
+
+// Template String ou Template Literal
