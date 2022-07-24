ffb95780a809e2291b799d20175eb5a1fdd6943c
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-07-24 15:31:30 -0300

    Tipos Primitivos

---
+++ type-number.js
@@ -0,0 +1,26 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = numeroPontoFlutuante / pontoFlutuanteSemZero
+
+console.log(novaOperacao)
+
+// Nan = Not a Number
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero)
