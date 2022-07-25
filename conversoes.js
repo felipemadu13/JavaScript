85d5794ba8f737295590c403d8d971c6c844ab10
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-07-25 08:14:39 -0300

    Aula 02

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numeroString = '456a';
+
+console.log(numero + Number(numeroString))
+
+
+// converão explicita
+
