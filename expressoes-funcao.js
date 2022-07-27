81317e2b5f7ba460e934d8332b2340f41871b89c
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-07-27 08:33:03 -0300

    Aula 05

---
+++ expressoes-funcao.js
@@ -0,0 +1,19 @@
+//declaração de função
+
+// function minhaFuncao(numero1, numero2) {
+//     return numero1 + numero2
+
+// }
+
+
+// expressão de função
+
+const soma = function(num1, num2) {return num1 + num2}
+// console.log(soma(1, 1))
+
+
+function apresentar() {
+    return 'Olá';
+}
+
+console.log(apresentar())
---
+++ parametros.js
@@ -0,0 +1,24 @@
+// parâmetros de função
+
+
+
+// console.log(soma(2, 2))
+
+
+// ordem dos parâmetros
+
+// function nomeIdade(nome, idade) {
+//     return `Meu nome é ${nome} e minha idade é ${idade}`
+// }
+
+// console.log(nomeIdade('felipe', 30))
+
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+function multiplica(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+console.log(multiplica(soma(4, 5), soma(3, 3)));
