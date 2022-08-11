0802e51952fab919b2fd5eea85023477b0381377
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-08-11 10:56:48 -0300

    Mudança de Pastas

---
+++ Guanabara_JavaScript/Aula12/ex012.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = agora.getHours()
+var minutos = agora.getMinutes()
+console.log(`Agora são exatamente ${hora} horas e ${minutos} minutos`)
+
+if (hora >= 8 && hora < 12) {
+    console.log('Bom dia!')
+} else if (hora >= 12 && hora < 18){
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa Noite!')
+}
