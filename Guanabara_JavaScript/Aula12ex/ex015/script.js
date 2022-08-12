ffa9a943dbdc84c70f0a724f09771e608122402d
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-08-12 08:39:23 -0300

    Manipulação de Pastas

---
+++ Guanabara_JavaScript/Aula12ex/ex015/script.js
@@ -0,0 +1,11 @@
+function verificar(){
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = window.document.getElementById('txtano');
+    var res = window.document.getElementById('res');
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente')
+    } else {
+        var fsex 
+    }
+}
