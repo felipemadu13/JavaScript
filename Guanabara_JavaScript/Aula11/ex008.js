33a7dc4d39c2793fc1b91831fda09846b0e9bd7e
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-08-18 11:15:17 -0300

    Mudança de Pastas

--- Guanabara_JavaScript/Aula11/ex008.js
+++
@@ -1,7 +0,0 @@
-var vel = 60.5;
-console.log(`A velocidade do seu carro é de ${vel}Km/h`);
-
-if (vel > 60) {
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
-}
-console.log('Dirija sempre usando o cinto de segurança!');
---
+++ Guanabara_JavaScript/Desafios/desafio01/script.js
@@ -0,0 +1,12 @@
+function clicar1() {
+    window.alert('Você clicou no 1ª botão');
+}
+
+function clicar2() {
+    window.alert('Você clicou no 2ª botão');
+}
+
+function clicar3() {
+    window.alert('Você clicou no 3ª botão');
+}
+
